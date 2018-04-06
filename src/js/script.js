2e649bc95809cc68715c4e36c344c61d84cc25c6
wararyo <wararyo@gmail.com>
2018-04-06 17:43:03 +0900

    スクロール位置によってnavigationの見た目が変わるやつ

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// 見てる見出しによってナビゲーションの見た目を変えるやつ
+var onScroll = function() {
+  var headings = document.getElementsByTagName('h2');
+  for (var i = headings.length - 1; i >= 0; i--) {
+  	var posY = headings[i].getBoundingClientRect().top;
+  	if(posY < 320) {
+  		navigation.currentSection = i;
+  		return;
+  	}
+  }
+  navigation.currentSection = -1;
+}
+document.addEventListener('scroll', onScroll)
+
+// おわり
+
+var navigation = new Vue({
+  el: '#nav',
+  data: {
+    currentSection: 0
+  }
+});
