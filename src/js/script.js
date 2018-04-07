86c550e7006a2fa15544f25f737d8cff1103dfe1
wararyo <wararyo@gmail.com>
2018-04-07 18:16:13 +0900

    動画のループが目立たなくなった

--- src/js/script.js
+++ src/js/script.js
@@ -22,8 +22,69 @@ var navigation = new Vue({
 
 // おわり
 
-var pauseMovie = function () {
-  var $player = document.getElementById('movie-iframe').contentWindow;
-  $player.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+// Youtube
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//ナビゲーション下の動画
+var moviePlayer;
+//電撃移動の動画 
+var etoPlayer;
+function onYouTubeIframeAPIReady() {
+  moviePlayer = new YT.Player('movie-player', {
+    height: '320',
+    width: '1280',
+    videoId: 'qmaJQN9-RVM',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      controls: 0,
+      showinfo: 0,
+      disablekb: 1,
+    }
+  });
+  etoPlayer = new YT.Player('about-player', {
+    height: '360',
+    width: '480',
+    videoId: 'xNKilogWNNs',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      controls: 0,
+      showinfo: 0,
+      disablekb: 1,
+      rel: 0
+    }
+  });
 }
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.mute();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    event.target.seekTo(0,true);
+    event.target.playVideo();
+  }
+}
+
+var pauseMovie = function () {
+  moviePlayer.pauseVideo();
+}
+
+//End Youtube
 
