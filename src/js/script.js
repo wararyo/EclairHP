22cabdf7e04938798b2c7281cc4a199d32b98b8f
wararyo <wararyo@gmail.com>
2018-05-17 22:18:46 +0900

    ヘッダーにパーティクル置いて背景変更

--- src/js/script.js
+++ src/js/script.js
@@ -151,3 +151,78 @@ var pauseMovie = function () {
 }
 
 //End Youtube
+
+console.log("hoge");
+
+
+// Stageオブジェクトを作成します。表示リストのルートになります。
+stage = new createjs.Stage("headerCanvas");
+ 
+// パーティクルシステム作成します。
+particleSystem = new particlejs.ParticleSystem();
+ 
+// パーティクルシステムの描画コンテナーを表示リストに登録します。
+stage.addChild(particleSystem.container);
+
+// Particle Developから保存したパラメーターを反映します。
+particleSystem.importFromJson(
+// JSONテキストのコピー＆ペースト ここから--
+{
+    "bgColor": "#00000",
+    "width": 1280,
+    "height": 640,
+    "emitFrequency": 20,
+    "startX": 900,
+    "startXVariance": 55,
+    "startY": 500,
+    "startYVariance": 155,
+    "initialDirection": "0",
+    "initialDirectionVariance": 270,
+    "initialSpeed": 1,
+    "initialSpeedVariance": 3,
+    "friction": "0.0035",
+    "accelerationSpeed": 0.005,
+    "accelerationDirection": 192,
+    "startScale": 0.2,
+    "startScaleVariance": 0.2,
+    "finishScale": "0",
+    "finishScaleVariance": "0",
+    "lifeSpan": "700",
+    "lifeSpanVariance": "500",
+    "startAlpha": "0.51",
+    "startAlphaVariance": "1",
+    "finishAlpha": "0",
+    "finishAlphaVariance": "0",
+    "shapeIdList": [
+        "blur_circle",
+        "circle"
+    ],
+    "startColor": {
+        "hue": "0",
+        "hueVariance": "0",
+        "saturation": "0",
+        "saturationVariance": 0,
+        "luminance": "100",
+        "luminanceVariance": "47"
+    },
+    "blendMode": true,
+    "alphaCurveType": "0",
+    "VERSION": "1.0.0"
+}
+// JSONテキストのコピー＆ペースト ここまで--
+);
+
+function handleTick() {
+  // パーティクルの発生・更新
+  particleSystem.update();
+ 
+  // 描画を更新する
+  stage.update();
+  console.log("hoge");
+}
+
+// フレームレートの設定
+createjs.Ticker.framerate = 60;
+// 定期的に呼ばれる関数を登録
+createjs.Ticker.on("tick", handleTick);
+
